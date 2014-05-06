5702cd0af6239dd197e33d03e8e96fa44b84590c
Russ Teabeault and Matt Farrar <rteabeault@rallydev.com>
2014-05-06 11:34:41 -0600

    Removed erroneous debug

--- lib/thocker/testing/serverspec.rb
+++ lib/thocker/testing/serverspec.rb
@@ -13,7 +13,6 @@ RSpec.configure do |c|
       options[:key_data] = [ENV['DOCKER_SSH_KEY']]
       options[:user] = 'root'
       options[:port] = ENV['DOCKER_SSH_PORT'] || 22
-      puts options.inspect
       c.ssh = Net::SSH.start(c.host, options[:user], options)
     end
   end
